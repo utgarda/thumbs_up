c4a9a64bfc5a44e0769d88443a95613602e067f3
Peter Boling <peter.boling@gmail.com>
2013-02-16 00:37:35 -0500

    Fixed typo

--- lib/thumbs_up.rb
+++ lib/thumbs_up.rb
@@ -25,7 +25,7 @@ module ThumbsUp
     end
 
     # The configuration object.
-    # @see I18::Airbrake.configure
+    # @see ThumbsUp::Configuration
     def configuration
       @configuration ||= Configuration.new
     end
