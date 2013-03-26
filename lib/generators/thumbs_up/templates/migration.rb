a30a72ab9b75164d7fbc27c080ddf250c3987e38
lvl0nax <lvl0nax@gmail.com>
2013-03-26 10:40:15 +0400

    change migration

--- lib/generators/thumbs_up/templates/migration.rb
+++ lib/generators/thumbs_up/templates/migration.rb
@@ -5,16 +5,19 @@ class ThumbsUpMigration < ActiveRecord::Migration
       t.boolean    :vote,     :default => false,    :null => false
       t.references :voteable, :polymorphic => true, :null => false
       t.references :voter,    :polymorphic => true
+      <% if options[:with_voting_tag] == true %>
+        t.references :voteable_tag,    :polymorphic => true
+      <% end %>
       t.timestamps
 
     end
 
     add_index :votes, [:voter_id, :voter_type]
-    add_index :votes, [:voteable_id, :voteable_type]
+    add_index :votes, [:voteable_id, :voteable_type, :voteable_tag]
 
 <% if options[:unique_voting] == true %>
     # Comment out the line below to allow multiple votes per voter on a single entity.
-    add_index :votes, [:voter_id, :voter_type, :voteable_id, :voteable_type], :unique => true, :name => 'fk_one_vote_per_user_per_entity'
+    add_index :votes, [:voter_id, :voter_type, :voteable_id, :voteable_type, :voteable_tag], :unique => true, :name => 'fk_one_vote_per_user_per_entity'
 <% end %>
   end
 
